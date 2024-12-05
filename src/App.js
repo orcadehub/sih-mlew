0ee41692775f66c0558b0bcf3920fa575a4a6d98
orcadehub <orcadehub2@gmail.com>
2024-12-05 18:07:26 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{textAlign:'center'}}>
+      <h3>Hi Bujjamma</h3>
+      <p>nikosam website chestanu today, ide website link</p>
+      <p>i will try my level best to make more user friendly sarena</p>
+      <p><i>Until then LOVE YOU BUJJI</i></p>
+      <br/>
+      <p>From your lovely</p>
+      <h2>AZZU</h2>
+    </div>
+  )
+}
+
+export default App
